08436ed24e1eab6a6e2078d8f48a31aa04bd9e71
sandip.mangale <sandip.m3@gmail.com>
2018-06-29 21:50:31 +0530

    first commit

---
+++ JBreak.java
@@ -0,0 +1,12 @@
+public class JBreak  
+{  
+public int cube(int a)  
+{  
+return a*a*a;  
+}  
+public static void main( String...jtp)  
+{  
+JBreak obj=new JBreak();  
+System.out.println("Cube of given number is : "+obj.cube(2));  
+}  
+}  
